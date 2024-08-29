dd35a6706ed5b723fe54711a41c5c5e3797c1d98
Isaias Junior <isaiscastro56@gmail.com>
2024-08-28 23:04:20 -0300

    calculando o valor da diaria

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -45,13 +45,13 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados <= 10)
             {
-                valor = 0;
+                valor = (10/100) * Suite.ValorDiaria;
             }
 
             return valor;
